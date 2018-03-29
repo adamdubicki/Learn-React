d590bbbb783a91604a221bf09a7a71e1c18afe49
adamdubicki <adam@freshworks.io>
2018-03-28 21:39:36 -0700

    Built Searchbar component

--- ReduxSimpleStarter-code/src/index.js
+++ ReduxSimpleStarter-code/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyB8JcWDVRVfMM9DD7o3rKGzo0fE98FLMsE';
+
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
